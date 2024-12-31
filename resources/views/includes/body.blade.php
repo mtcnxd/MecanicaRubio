bf5174465396a113e08be2c9ad3778e34b3b8cc2
Marcos Tzuc <mtc.nxd@gmail.com>
2024-12-31 10:50:03 -0600

    Footer

--- resources/views/includes/body.blade.php
+++ resources/views/includes/body.blade.php
@@ -16,7 +16,9 @@
     @yield('menu')
     @yield('body')
     @yield('content')
-    @yield('footer')
+    
+    @include('includes.footer')
+
     @yield('modal')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,3 @@
-<div>
-    this is the footer
+<div class="main-content text-center pb-3">
+    <p class="fs-7">Ingenieria Mécanica Rúbio @AllRigths Reserved 2025/2026</p>
 </div>
