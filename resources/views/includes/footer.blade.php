a701aa035dad76f5ad9a94c52a2ae9a889d311f1
Marcos Tzuc <mtc.nxd@gmail.com>
2025-02-27 17:54:33 -0600

    Some styling improve

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,5 @@
-<div class="main-content text-center pb-3">
-    <p class="fs-7">Ingenieria Mécanica Rúbio @AllRigths Reserved 2025/2026</p>
+<div class="main-content text-center pb-4">
+    <p class="fs-7">
+        <!-- Ingenieria Mécanica Rúbio @AllRigths Reserved 2025/2026 -->
+    </p>
 </div>
