9b9e99639d948621e540c0a83e4845d32ccae7aa
Marcos Tzuc Cen <marcos@virtualia.com>
2025-07-10 16:25:41 -0600

    Add avatar and Google authentication fields to users table; implement Google login in routes

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\Dashboard\Finance;
 use App\Http\Controllers\Dashboard\Settings;
 use App\Http\Controllers\Auth\Login;
 use Carbon\Carbon;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,62 +27,82 @@ use Carbon\Carbon;
 |
 */
 
-
+// Login and registration routes
 Route::get('/auth/redirect', function () {
     return Socialite::driver('google')->redirect();
 });
  
 Route::get('/auth/callback', function () {
-    $user = Socialite::driver('google')->user();
-});
+    try {
+        $googleUser = Socialite::driver('google')->user();
 
+        $user = User::updateOrCreate([
+            'email'         => $googleUser->email,
+        ], [
+            'name'          => $googleUser->name,
+            'avatar'        => $googleUser->avatar,
+            'google_token'  => $googleUser->token,
+            'google_userid' => $googleUser->id,
+        ]);
+    
+        Auth::login($user);
 
-Route::get('/', function(){
-    return view('content');
+        return to_route('services.index');
+    }
+    catch (Exception $err) {
+        print_r(
+            sprintf("Error: %s", $err->getMessage())
+        );
+    }
 });
 
 Route::get('admin', [Login::class, 'index'])->name('login');
 Route::post('admin', [Login::class, 'login']);
 Route::post('admin/logout', [Login::class, 'logout'])->name('logout');
+// Login and registration routes
+
+Route::get('/', function(){
+    return view('content');
+});
 
 Route::group(['prefix' => 'client'], function(){
     // Customer routes
 });
 
 Route::middleware(['auth'])->group( function ()
-{
-    Route::group(['prefix' => 'admin'], function(){
-        
-        Route::resource('services', Services::class);
-        Route::resource('clients', Clients::class);
-        Route::resource('cars', Cars::class);
-        Route::resource('expenses', Expenses::class);
-        Route::resource('payroll', Payroll::class);
-        Route::resource('employees', Employees::class);        
+    {
+        Route::group(['prefix' => 'admin'], function()
+            {
+                Route::resource('services', Services::class);
+                Route::resource('clients', Clients::class);
+                Route::resource('cars', Cars::class);
+                Route::resource('expenses', Expenses::class);
+                Route::resource('payroll', Payroll::class);
+                Route::resource('employees', Employees::class);        
 
-        Route::get('quote', [Services::class, 'createQuote'])->name('quote.create');
-        Route::get('calendar', [Calendar::class, 'index'])->name('calendar.index');
-        Route::get('dashboard', [Services::class, 'dashboard'])->name('dashboard.index');
-        Route::get('profile', [Employees::class, 'profileIndex'])->name('profile.index');
+                Route::get('quote', [Services::class, 'createQuote'])->name('quote.create');
+                Route::get('calendar', [Calendar::class, 'index'])->name('calendar.index');
+                Route::get('dashboard', [Services::class, 'dashboard'])->name('dashboard.index');
+                Route::get('profile', [Employees::class, 'profileIndex'])->name('profile.index');
 
-        # Reports
-        Route::get('reports/employees/{userid}', [Employees::class, 'report'])->name('reports.employees');
-        Route::get('reports/employees', [Employees::class, 'report'])->name('reports.employees');
-        Route::get('reports/balance', [Expenses::class, 'report'])->name('reports.balance');
-        Route::get('reports/autos', [Cars::class, 'report'])->name('reports.autos');
-        Route::get('finance/{client}', [Finance::class, 'show'])->name('finance');
+                # Reports
+                Route::get('reports/employees/{userid}', [Employees::class, 'report'])->name('reports.employees');
+                Route::get('reports/employees', [Employees::class, 'report'])->name('reports.employees');
+                Route::get('reports/balance', [Expenses::class, 'report'])->name('reports.balance');
+                Route::get('reports/autos', [Cars::class, 'report'])->name('reports.autos');
+                Route::get('finance/{client}', [Finance::class, 'show'])->name('finance');
+            }
+        );
 
-    });
 
+        Route::post('admin/profile', [Employees::class, 'profileUpdate'])->name('profile.update');
 
-    Route::post('admin/profile', [Employees::class, 'profileUpdate'])->name('profile.update');
+        Route::get('admin/settings', [Settings::class, 'index'])->name('setting.index');
 
-    Route::get('admin/settings', [Settings::class, 'index'])->name('setting.index');
+        Route::post('admin/settings', [Settings::class, 'update'])->name('setting.update');
 
-    Route::post('admin/settings', [Settings::class, 'update'])->name('setting.update');
+        Route::post('admin/settings/create', [Settings::class, 'store'])->name('setting.store');
 
-    Route::post('admin/settings/create', [Settings::class, 'store'])->name('setting.store');
-
-    Route::get('admin/sendEmailInvoice/{service}', [Services::class, 'sendEmailInvoice'])->name('sendEmailInvoice');
-
-});
+        Route::get('admin/sendEmailInvoice/{service}', [Services::class, 'sendEmailInvoice'])->name('sendEmailInvoice');
+    }
+);
