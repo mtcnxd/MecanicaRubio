0058525f8b20154bb8fb014b736b1b6ddb866422
Marcos Tzuc Cén <mtc.nxd@gmail.com>
2025-12-12 23:13:53 -0600

    Actualizar Investment.php

--- app/Models/Investment.php
+++ app/Models/Investment.php
@@ -52,7 +52,7 @@ class Investment extends Model
     public static function getInvestmentAmountMonthAgo()
     {
         $first = self::getAmountByDaysAgo(1);
-        $last  = self::getAmountByDaysAgo(13);
+        $last  = self::getAmountByDaysAgo(16);
 
         return  ($first - $last);
     }
@@ -60,7 +60,7 @@ class Investment extends Model
     public static function getInvestmentPercentageMonthAgo()
     {
         $first = self::getAmountByDaysAgo(1);
-        $last  = self::getAmountByDaysAgo(13);
+        $last  = self::getAmountByDaysAgo(16);
 
         return Helpers::convertToPercentage($first, $last);
     }
