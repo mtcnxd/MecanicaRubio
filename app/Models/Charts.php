1c17e5cde5408b9305b96883af14940af3871162
Marcos Tzuc Cen <marcos@virtualia.com>
2026-01-06 14:27:32 -0600

    limit 30

--- app/Models/Charts.php
+++ app/Models/Charts.php
@@ -84,6 +84,7 @@ class Charts extends Model
             ->select(DB::raw('sum(amount) as amount, export_date'))
             ->groupBy('export_date')
             ->orderBy('export_date', 'asc')
+            ->limit(30)
             ->get();
 
         foreach($data as $value){
